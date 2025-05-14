7fc6e80550311f5ae87b8ed0f1b9d2b320c86bce
pocketopii <rlwns00@gmail.com>
2025-05-14 15:12:17 +0900

    메인 통합합

--- myproject/config.py
+++ myproject/config.py
@@ -3,7 +3,7 @@ import pymysql
 DB_CONFIG = {
     'host': 'localhost',
     'user': 'root',
-    'password': 'comep1522w',
-    'database': 'sm',
+    'password': '1234',
+    'database': 'smartfarm',
     'charset': 'utf8mb4'
 }
