b4c1d22c5a0db60429cade5a37ec42f3753beb6c
pyodolski <pjwp0928w@gmail.com>
2025-06-04 14:15:31 +0900

    db update

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import pymysql
 DB_CONFIG = {
     'host': 'localhost',
     'user': 'root',
-    'password': 'q1w2e3r4^',
-    'database': 'smart_farm',
+    'password': 'comep1522w',
+    'database': 'sm',
     'charset': 'utf8mb4'
 }
